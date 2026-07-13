a91cab6aa2dfe0eb0e72ff5575b62c063be02326
Julianna Vaccaro <julianna.vicente@icloud.com>
2026-07-13 19:58:20 -0400

    added coding structure for background color and dom heading

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 import "./styles.css";
-let mainEl = document.querySelector("main")
+let mainEl = document.querySelector("main");
+
+mainEl.style.backgroundColor = var--main-bg// i dont know
+
+mainEl.innerHTML ="<h1> DOM Manipulation</h1>" //i also dont know 
