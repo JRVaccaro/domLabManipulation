82164b913aff55c8021a0e77c43a0cd1ecba10b0
Julianna Vaccaro <julianna.vicente@icloud.com>
2026-07-13 20:03:47 -0400

    Cacheing top menu element

--- index.js
+++ index.js
@@ -20,3 +20,5 @@ mainEl.style.backgroundColor = var--main-bg// i dont know
 mainEl.innerHTML ="<h1> DOM Manipulation</h1>" //i also dont know 
 
 mainEl.classList.add("flex-ctr"); 
+
+let topMenuEl = document.getElementById("top-menu");
