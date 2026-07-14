e97c8b22cec96cdb5ca265e356ae7941198cba44
Julianna Vaccaro <julianna.vicente@icloud.com>
2026-07-13 20:02:19 -0400

    Added menu data

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 import "./styles.css";
+// Menu data structure
+
+var menuLinks = [
+
+  { text: 'about', href: '/about' },
+
+  { text: 'catalog', href: '/catalog' },
+
+  { text: 'orders', href: '/orders' },
+
+  { text: 'account', href: '/account' },
+
+];
+
 let mainEl = document.querySelector("main");
 
 mainEl.style.backgroundColor = var--main-bg// i dont know
